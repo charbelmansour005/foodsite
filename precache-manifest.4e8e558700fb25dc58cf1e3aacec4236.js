63261a090e2abd440294bfaf53007dbce80aea37
Mario <mario.attieh.2@gmail.com>
2022-08-19 16:15:52 +0300

    Updates

---
+++ precache-manifest.4e8e558700fb25dc58cf1e3aacec4236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ca810ca26cf62cf7a603c8712012c7",
+    "url": "/foodsite/index.html"
+  },
+  {
+    "revision": "9bd8bd8a53ff292abe0f",
+    "url": "/foodsite/static/css/main.01a71211.chunk.css"
+  },
+  {
+    "revision": "d81876df43df90c4cb8b",
+    "url": "/foodsite/static/js/2.bcc724ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodsite/static/js/2.bcc724ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd8bd8a53ff292abe0f",
+    "url": "/foodsite/static/js/main.eb401e0d.chunk.js"
+  },
+  {
+    "revision": "91e6d856f8f7d3f6dcac",
+    "url": "/foodsite/static/js/runtime-main.40b0a4b4.js"
+  }
+]);
