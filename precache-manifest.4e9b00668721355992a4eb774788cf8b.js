eaa28721d9171bf3422d0be57d1a013e1773a7b3
Mario <mario.attieh.2@gmail.com>
2022-08-19 12:58:35 +0300

    Updates

---
+++ precache-manifest.4e9b00668721355992a4eb774788cf8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768b1d519f6a5552f3b9eaeb56d6bfa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bd8bd8a53ff292abe0f",
+    "url": "/static/css/main.01a71211.chunk.css"
+  },
+  {
+    "revision": "d81876df43df90c4cb8b",
+    "url": "/static/js/2.bcc724ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bcc724ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd8bd8a53ff292abe0f",
+    "url": "/static/js/main.eb401e0d.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
